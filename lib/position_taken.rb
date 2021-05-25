6377fddafa17c643082c846db7e48c35e00c5719
Latella L <lt.latella@gmail.com>
2021-05-25 23:25:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,7 @@
 # code your #position_taken? method here!
  #position_taken? in lib/position_taken.rb accepts a board and the index tocheck as arguments
  
+ 
+ #position_taken? in lib/position_taken.rb returns false if the board has an empty string " " in the desired index
+ 
+ 
